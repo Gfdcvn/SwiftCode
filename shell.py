a514a12f2c7f113a5f374a35053072c1ef5e733b
Richard cool(rbgrm) <richardsonnomad@gmail.com>
2024-12-30 16:53:55 +0200

    V6: Added logic and comparison

--- shell.py
+++ shell.py
@@ -1,6 +1,7 @@
 import swiftcode
 
-print(f'Running Swift Code Interactive Shell, Swift Code V0.4')
+print(f'Running Swift Code Interactive Shell, Swift Code V0.6')
+print(f"Type 'info' for info about the shell, type 'exit' to exit the shell")
 while True:
     text = input('SwiftCode > ')
     result, error = swiftcode.run('<stdin>', text)
--- swiftcode.py
+++ swiftcode.py
@@ -38,7 +38,9 @@ class IllegalCharError(Error):
 class InvalidSyntaxError(Error):
     def __init__(self, pos_start, pos_end, details):
         super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
-
+class ExpectedCharError(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, 'Expected Character', details)
 class RTError(Error):
     def __init__(self, pos_start, pos_end, details, context):
         super().__init__(pos_start, pos_end, 'Runtime Error', details)
@@ -106,10 +108,19 @@ ST_POW = 'POW'
 ST_EQ = 'EQ'
 ST_LPAREN = 'LPAREN'
 ST_RPAREN = 'RPAREN'
+ST_EE = 'EE'
+ST_NE = 'NE'
+ST_LT = 'LT'
+ST_GT = 'GT'
+ST_LTE = 'LTE'
+ST_GTE = 'GTE'
 ST_EOF = 'EOF'
 
 KEYWORDS = [
-    'variable'
+    'variable', # Variable Declaration
+    'and', # Logical
+    'or', # Logical
+    'not' # Logical
 ]
 
 class Token:
@@ -172,15 +183,23 @@ class Lexer:
             elif self.current_char == '^':
                 tokens.append(Token(ST_POW, pos_start=self.pos))
                 self.advance()
-            elif self.current_char == '=':
-                tokens.append(Token(ST_EQ, pos_start=self.pos))
-                self.advance()
+
             elif self.current_char == '(':
                 tokens.append(Token(ST_LPAREN, pos_start=self.pos))
                 self.advance()
             elif self.current_char == ')':
                 tokens.append(Token(ST_RPAREN, pos_start=self.pos))
                 self.advance()
+            elif self.current_char == '!':
+                tok, error = self.make_not_equals()
+                if error: return [], error
+                tokens.append(tok)
+            elif self.current_char == '=':
+                tokens.append(self.make_equals())
+            elif self.current_char == '<':
+                tokens.append(self.make_less_than())
+            elif self.current_char == '>':
+                tokens.append(self.make_greater_than())
             else:
                 pos_start = self.pos.copy()
                 char = self.current_char
@@ -217,6 +236,46 @@ class Lexer:
 
         tok_type = ST_KEYWORD if id_str in KEYWORDS else ST_IDENTIFIER
         return Token(tok_type, id_str, pos_start, self.pos)
+    def make_not_equals(self):
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            return Token(ST_NE, pos_start=pos_start, pos_end=self.pos), None
+        self.advance()
+        return None, ExpectedCharError(pos_start, self.pos, "'=' after '!'")
+    
+    def make_equals(self):
+        tok_type = ST_EQ
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            tok_type = ST_EE
+
+        return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+    def make_less_than(self):
+        tok_type = ST_LT
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            tok_type = ST_LTE
+
+        return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+    def make_greater_than(self):
+        tok_type = ST_GT
+        pos_start = self.pos.copy()
+        self.advance()
+
+        if self.current_char == '=':
+            self.advance()
+            tok_type = ST_GTE
+
+        return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
 
 
 ##############################################
@@ -375,6 +434,29 @@ class Parser:
 
     def term(self):
         return self.bin_op(self.factor, (ST_MUL,ST_DIV))
+    
+    def arith_expr(self):
+        return self.bin_op(self.term, (ST_PLUS, ST_MINUS))
+
+    def comp_expr(self):
+        res = ParseResult()
+
+        if self.current_tok.matches(ST_KEYWORD, 'not'):
+            op_tok = self.current_tok
+            res.register_advancment()
+            self.advance()
+
+            node = res.register(self.comp_expr())
+            if res.error: return res
+            return res.success(UnaryOpNode(op_tok, node))
+        node = res.register(self.bin_op(self.arith_expr, ((ST_EE, ST_NE, ST_LT, ST_GT, ST_LTE, ST_GTE))))
+
+        if res.error:
+            return res.failiure(InvalidSyntaxError(
+                self.current_tok.pos_start, self.current_tok.pos_end,
+                "Expected an int, a float, an identifier, '+', '-', '(', 'not'"
+            ))
+        return res.success(node)
 
     def expr(self):
         res = ParseResult()
@@ -403,7 +485,7 @@ class Parser:
             return res.success(VarAssignNode(var_name, expr))
 
 
-        node =  res.register(self.bin_op(self.term, (ST_PLUS,ST_MINUS)))
+        node =  res.register(self.bin_op(self.comp_expr, ((ST_KEYWORD, 'and'), (ST_KEYWORD, 'or'))))
 
         if res.error: 
             return res.failiure(InvalidSyntaxError(
@@ -419,7 +501,7 @@ class Parser:
         left = res.register(func_a())
         if res.error: return res
 
-        while self.current_tok.type in ops:
+        while self.current_tok.type in ops or (self.current_tok.type, self.current_tok.value) in ops: 
             op_tok = self.current_tok
             res.register_advancment()
             self.advance()
@@ -495,6 +577,33 @@ class Number:
     def powed_by(self, other):
         if isinstance(other, Number):
             return Number(self.value ** other.value).set_context(self.context), None
+        
+    def get_comparison_eq(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value == other.value)).set_context(self.context), None
+    def get_comparison_ne(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value != other.value)).set_context(self.context), None
+    def get_comparison_lt(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value < other.value)).set_context(self.context), None
+    def get_comparison_gt(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value > other.value)).set_context(self.context), None
+    def get_comparison_lte(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value <= other.value)).set_context(self.context), None
+    def get_comparison_gte(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value >= other.value)).set_context(self.context), None
+    def anded_by(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value and other.value)).set_context(self.context), None
+    def ored_by(self, other):
+        if isinstance(other, Number):
+            return Number(int(self.value or other.value)).set_context(self.context), None
+    def notted(self):
+        return Number(1 if self.value == 0 else 0).set_context(self.context), None
 
     def copy(self):
         copy = Number(self.value)
@@ -596,6 +705,22 @@ class Interpreter:
             result, error = left.dived_by(right)
         elif node.op_tok.type == ST_POW:
             result, error = left.powed_by(right)
+        elif node.op_tok.type == ST_EE:
+            result, error = left.get_comparison_eq(right)
+        elif node.op_tok.type == ST_NE:
+            result, error = left.get_comparison_ne(right)
+        elif node.op_tok.type == ST_LT:
+            result, error = left.get_comparison_lt(right)
+        elif node.op_tok.type == ST_GT:
+            result, error = left.get_comparison_gt(right)
+        elif node.op_tok.type == ST_LTE:
+            result, error = left.get_comparison_lte(right)
+        elif node.op_tok.type == ST_GTE:
+            result, error = left.get_comparison_gte(right)
+        elif node.op_tok.matches(ST_KEYWORD, 'and'):
+            result, error = left.anded_by(right)
+        elif node.op_tok.matches(ST_KEYWORD, 'or'):
+            result, error = left.ored_by(right)
 
         if error:
             return res.failiure(error)
@@ -611,6 +736,8 @@ class Interpreter:
 
         if node.op_tok.type == ST_MINUS:
             number, error = number.multed_by(Number(-1))
+        elif node.op_tok.matches(ST_KEYWORD, 'not'):
+            number, error = number.notted()
 
         if res.error:
             return res.failiure(error)
@@ -622,7 +749,8 @@ class Interpreter:
 
 global_symbol_table = SymbolTable()
 global_symbol_table.set("null", Number(0))
-
+global_symbol_table.set("true", Number(1))
+global_symbol_table.set("false", Number(0))
 
 def run(fn, text):
     # Gen Tokens
