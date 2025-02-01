a832efe793b2e1634701737597e09529d3a7de3e
Richard cool(rbgrm) <richardsonnomad@gmail.com>
2025-02-01 17:53:22 +0200

    Fixing errors

--- swiftcode.py
+++ swiftcode.py
@@ -60,7 +60,7 @@ class RTError(Error):
         ctx = self.context
 
         while ctx:
-            #result = f'File: {pos.fn}, Line: {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+            result = f'File: {self.pos_start.fn}, Line: {str(self.pos_end.ln + 1)}, in {ctx.display_name}\n' + result
             pos = ctx.parent_entry_pos
             ctx = ctx.parent
 
